615f6506fb9298480034c976d10dcae0b0464d8e
Denzel <dbartolaba123@gmail.com>
2022-02-18 09:50:49 -0800

    Finished with my AuthPage

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import { signIn, signUp } from './services/fetch-utils';
 
 export default function AuthPage({ setCurrentUser }) {
-  const [signInemail, setSignInEmail] = useState('');
-  const [signInpassword, setSignInPassword] = useState('');
+  const [signInEmail, setSignInEmail] = useState('');
+  const [signInPassword, setSignInPassword] = useState('');
   const [signUpEmail, setSignUpEmail] = useState('');
   const [signUpPassword, setSignUpPassword] = useState('');
 
@@ -26,11 +26,38 @@ export default function AuthPage({ setCurrentUser }) {
   async function handleSignIn(e) {
     e.preventDefault();
 
-    const user = await signIn(signInemail, signInpassword);
+    const user = await signIn(signInEmail, signInPassword);
     setCurrentUser(user);
     clearForms();
   }
   return (
-    <div>AuthPage</div>
+    <div className='auth-page'>
+      <form onSubmit={handleSignUp}>
+        <label>
+                Email
+
+          <input value={signUpEmail} onChange={e => setSignUpEmail(e.target.value)} />
+        </label>
+        <label>
+                Password
+
+          <input value={signUpPassword} onChange={e => setSignUpPassword(e.target.value)} />
+        </label>
+        <button>Sign Up</button>
+      </form>
+      <form onSubmit={handleSignIn}>
+        <label>
+                Email
+
+          <input value={signInEmail} onChange={e => setSignInEmail(e.target.value)} />
+        </label>
+        <label>
+                Password
+
+          <input value={signInPassword} onChange={e => setSignInPassword(e.target.value)} />
+        </label>
+        <button>Sign In</button>
+      </form>
+    </div>
   );
 }
